5281d61ec8b9669d91cbf8be3d6d0af209c804e2
Black | Douxx <136928935+douxxu@users.noreply.github.com>
2024-01-16 01:48:35 +0100

    args supported + DouServer.js

--- cmd.js
+++ cmd.js
@@ -3,9 +3,11 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 
-const sp = path.join(__dirname, 'server.js');
+const scriptPath = path.join(__dirname, 'DouServer.js');
 
-const result = spawnSync('node', [sp], { stdio: 'inherit' });
+const args = process.argv.slice(2);
+
+const result = spawnSync('node', [scriptPath, ...args], { stdio: 'inherit' });
 
 if (result.error) {
   console.error(result.error.message);
